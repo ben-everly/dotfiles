65c435e8b572ba4aa599bdccd13d8d43916b3abf
Ben Everly <ben.everly@oberd.com>
2016-01-29 11:53:35 -0600

    dont look at .min.js files with grep

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,7 +4,7 @@ alias ls='ls -F'
 alias ll='ls -l'
 alias la='ls -al'
 
-alias grep='grep --color=always'
+alias grep='grep --color=always --exclude="*.min.js"'
 alias less='less -rSFX'
 
 alias :q='exit'
