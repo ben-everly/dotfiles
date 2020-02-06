7cfebf41d02c8899ac6533973178004bb7827704
Ben Everly <ben.everly@oberd.com>
2020-02-06 16:10:07 -0600

    color fixes for ag and ls

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
+export LESS='RSFXm'
 
-alias ls='ls -F --color=auto'
+alias ls='ls -F --color=always'
 alias ll='ls -l'
 alias la='ls -al'
 
-alias grep='grep --color=auto --exclude=*.min.js --exclude-dir=.git'
-alias less='less -rSFX'
+alias grep='grep --color=always --exclude=*.min.js --exclude-dir=.git'
+alias ag='ag --pager="less"'
 
 alias :q='exit'
 
