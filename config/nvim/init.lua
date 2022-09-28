bdfba519f7acbda0a2a2e80dc32509a4a142b6b8
Ben Everly <ben.everly@oberd.com>
2022-09-28 15:19:48 -0500

    use lua to configure nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,17 +1,18 @@
+vim.cmd([[
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 let mapleader = " "
 
 if empty(glob('~/.vim/autoload/plug.vim'))
-    silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-        \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+	\ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 endif
 
 augroup plug
-    au!
-    autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
-        \| PlugInstall --sync | source $MYVIMRC
-        \| endif
+	au!
+	autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
+		\| PlugInstall --sync | source $MYVIMRC
+		\| endif
 augroup end
 
 call plug#begin('~/.vim/plug')
@@ -35,32 +36,32 @@ vmap <leader>gh :'<,'>GBrowse<cr>
 
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
 let g:coc_global_extensions = [
-            \ 'coc-snippets',
-            \ 'coc-lists',
-            \ 'coc-git',
-            \ 'coc-phpls',
-            \ 'coc-php-cs-fixer',
-            \ 'coc-go',
-            \ 'coc-tsserver',
-            \ 'coc-eslint',
-            \ 'coc-sql',
-            \ 'coc-json',
-            \ 'coc-yaml',
-            \ 'coc-xml',
-            \ 'coc-html',
-            \ 'coc-css',
-            \ 'coc-sh',
-            \ 'coc-docker',
-            \ 'coc-vimlsp',
-            \ 'coc-lua',
-            \ 'coc-markdownlint',
-            \ 'coc-markdown-preview-enhanced',
-            \ ]
-" \ 'coc-spell-checker',
-" \ 'coc-phpstan',
-" \ 'coc-jsonc',
-" \ '@yeagassy/coc-tailwindcss3',
-" \ '@yeagassy/coc-ansible',
+	\ 'coc-snippets',
+	\ 'coc-lists',
+	\ 'coc-git',
+	\ 'coc-phpls',
+	\ 'coc-php-cs-fixer',
+	\ 'coc-go',
+	\ 'coc-tsserver',
+	\ 'coc-eslint',
+	\ 'coc-sql',
+	\ 'coc-json',
+	\ 'coc-yaml',
+	\ 'coc-xml',
+	\ 'coc-html',
+	\ 'coc-css',
+	\ 'coc-sh',
+	\ 'coc-docker',
+	\ 'coc-vimlsp',
+	\ 'coc-lua',
+	\ 'coc-markdownlint',
+	\ 'coc-markdown-preview-enhanced',
+\ ]
+	" \ 'coc-spell-checker',
+	" \ 'coc-phpstan',
+	" \ 'coc-jsonc',
+	" \ '@yeagassy/coc-tailwindcss3',
+	" \ '@yeagassy/coc-ansible',
 set shortmess+=c
 nmap <silent> [e <Plug>(coc-diagnostic-prev)
 nmap <silent> ]e <Plug>(coc-diagnostic-next)
@@ -70,32 +71,32 @@ nmap <silent> gy <Plug>(coc-type-definition)
 nmap <silent> gi <Plug>(coc-implementation)
 nmap <silent> gr <Plug>(coc-references)
 imap <silent><expr> <TAB>
-            \ coc#pum#visible() ? coc#pum#next(1):
-            \ pumvisible() ? "\<C-n>" :
-            \ coc#jumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>":
-            \ PreviousCharIsWhitespace() ? "\<TAB>" :
-            \ coc#refresh()
+	\ coc#pum#visible() ? coc#pum#next(1):
+	\ pumvisible() ? "\<C-n>" :
+	\ coc#jumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>":
+	\ PreviousCharIsWhitespace() ? "\<TAB>" :
+	\ coc#refresh()
 imap <expr><S-TAB>
-            \ coc#pum#visible() ? coc#pum#prev(1):
-            \ pumvisible() ? "\<C-p>":
-            \ "\<C-h>"
+	\ coc#pum#visible() ? coc#pum#prev(1):
+	\ pumvisible() ? "\<C-p>":
+	\ "\<C-h>"
 function! PreviousCharIsWhitespace() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~# '\s'
+let col = col('.') - 1
+return !col || getline('.')[col - 1]  =~# '\s'
 endfunction
 inoremap <silent><expr> <c-space> coc#pum#visible() ? coc#pum#cancel(): coc#refresh()
 inoremap <silent><expr> <cr>
-            \ coc#pum#visible() ? coc#_select_confirm():
-            \ pumvisible() ? coc#_select_confirm():
-            \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
-            \ "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+	\ coc#pum#visible() ? coc#_select_confirm():
+	\ pumvisible() ? coc#_select_confirm():
+	\ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
+	\ "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
 nnoremap <silent> K :call ShowDocumentation()<CR>
 function! ShowDocumentation()
-    if CocAction('hasProvider', 'hover')
-        call CocActionAsync('doHover')
-    else
-        call feedkeys('K', 'in')
-    endif
+if CocAction('hasProvider', 'hover')
+	call CocActionAsync('doHover')
+else
+	call feedkeys('K', 'in')
+endif
 endfunction
 autocmd CursorHold * silent call CocActionAsync('highlight')
 nmap <leader>rn <Plug>(coc-rename)
@@ -115,17 +116,17 @@ omap ic <Plug>(coc-classobj-i)
 xmap ac <Plug>(coc-classobj-a)
 omap ac <Plug>(coc-classobj-a)
 if has("nvim-0.5.0") || has("patch-8.1.1564")
-    set signcolumn=number
+	set signcolumn=number
 else
-    set signcolumn=yes
+	set signcolumn=yes
 endif
 if has('nvim-0.4.0') || has('patch-8.2.0750')
-    nnoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-    nnoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-    inoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-    inoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
-    vnoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-    vnoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+	nnoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+	nnoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+	inoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+	inoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+	vnoremap <silent><nowait><expr> <C-j> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+	vnoremap <silent><nowait><expr> <C-k> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
 endif
 nmap <silent> <C-s> <Plug>(coc-range-select)
 xmap <silent> <C-s> <Plug>(coc-range-select)
@@ -202,55 +203,6 @@ call plug#end()
 call coc#config("intelephense.licenceKey", $INTELEPHENSE_KEY)
 
 lua << EOF
-local dap, dapui = require('dap'), require('dapui')
-dap.adapters.php = {
-    type = 'executable',
-    command = 'node',
-    args = { '/home/beneverly/.ansible/pull/work-XPS-15-9520/tmp/php-debug-client/out/phpDebug.js' }
-    }
-
-dap.configurations.php = {
-    {
-            type = 'php',
-            request = 'launch',
-            name = 'Listen for Xdebug',
-            port = 9000,
-            log = true,
-            pathMappings = {
-                ['/var/www/app/'] = '${workspaceFolder}'
-                }
-}
-}
-
-dapui.setup()
-dap.listeners.after.event_initialized["dapui_config"] = function()
-dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-dapui.close()
-end
-
-require'nvim-treesitter.configs'.setup {
-    auto_install = true,
-    highlight = {
-        enable = true,
-        },
-    indent = {
-        enable = true,
-        },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-            },
-        },
-    }
 EOF
 nmap <leader>d<cr> :lua require'dap'.continue()<CR>
 nmap <leader>db :lua require'dap'.toggle_breakpoint()<CR>
@@ -285,8 +237,8 @@ set inccommand=nosplit
 let g:vimsyn_embed = 'l'
 
 augroup folding
-    au!
-    autocmd Syntax * normal zR
+au!
+autocmd Syntax * normal zR
 augroup end
 
 xnoremap il g_o^
@@ -309,3 +261,53 @@ vmap // y/\V<c-r>=escape(@",'/\')<cr><cr>
 cmap w!! w !sudo tee % >/dev/null
 
 runtime vimrc.local
+]])
+local dap, dapui = require('dap'), require('dapui')
+dap.adapters.php = {
+	type = 'executable',
+	command = 'node',
+	args = { '/home/beneverly/.ansible/pull/work-XPS-15-9520/tmp/php-debug-client/out/phpDebug.js' }
+}
+
+dap.configurations.php = {
+	{
+		type = 'php',
+		request = 'launch',
+		name = 'Listen for Xdebug',
+		port = 9000,
+		log = true,
+		pathMappings = {
+			['/var/www/app/'] = '${workspaceFolder}'
+		}
+	}
+}
+
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+require'nvim-treesitter.configs'.setup {
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+}
