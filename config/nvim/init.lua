9f9502bb3f6de10b73ae06072bce7fc084e667ac
Ben Everly <ben.everly@oberd.com>
2022-10-01 22:44:59 -0500

    move augroup for unfolding to lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -260,12 +260,10 @@ vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.g.vimsyn_embed = 'l'
 vim.cmd('colorscheme gruvbox')
 
-vim.cmd([[
+local folding_group = vim.api.nvim_create_augroup('folding', {clear = true})
+vim.api.nvim_create_autocmd('Syntax', { pattern = '*', group = folding_group, command = 'normal zR' })
 
-augroup folding
-au!
-autocmd Syntax * normal zR
-augroup end
+vim.cmd([[
 
 xnoremap il g_o^
 onoremap il :normal vil<CR>
