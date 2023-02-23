bbf9b15e16b17dcadcff1d6fa8a31d0ce6f94f9a
Ben Everly <ben.everly@oberd.com>
2023-02-23 10:09:49 -0600

    prioritize find_files over git_files.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -205,8 +205,8 @@ map('n', '<leader>cu', '<Plug>Commentary<Plug>Commentary')
 
 -- telescope settings
 local builtin = require('telescope.builtin')
-map('n', '<leader>ff', builtin.git_files)
-map('n', '<leader>fF', builtin.find_files)
+map('n', '<leader>ff', builtin.find_files)
+map('n', '<leader>fF', builtin.git_files)
 map('n', '<leader>fg', builtin.live_grep)
 map('n', '<leader>fb', builtin.buffers)
 map('n', '<leader>fh', builtin.help_tags)
