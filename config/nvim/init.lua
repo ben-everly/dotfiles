a9fbc3f02d813e659ce50d6913853553d3aa8a57
Ben Everly <ben.everly@oberd.com>
2023-06-24 20:19:19 -0500

    update incremental selection keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -348,9 +348,9 @@ require'nvim-treesitter.configs'.setup {
 		enable = true,
 		keymaps = {
 			init_selection = "gnn",
-			node_incremental = "grn",
 			scope_incremental = "grc",
-			node_decremental = "grm",
+			node_incremental = ";",
+			node_decremental = ",",
 		},
 	},
 }
