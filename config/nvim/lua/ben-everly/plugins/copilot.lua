bc7c313b65d83a2b8388cd87efee2de4e5e7c3eb
Ben Everly <ben.everly@oberd.com>
2023-07-03 14:47:53 -0500

    copilot module returns table

--- config/nvim/lua/ben-everly/plugins/copilot.lua
+++ config/nvim/lua/ben-everly/plugins/copilot.lua
@@ -1,5 +1,8 @@
-return function()
-	vim.g.copilot_no_tab_map = true
-	vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("")',
-		{ noremap = true, silent = true, expr = true, replace_keycodes = false })
-end
+return {
+	'github/copilot.vim',
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("")',
+			{ noremap = true, silent = true, expr = true, replace_keycodes = false })
+	end
+}
