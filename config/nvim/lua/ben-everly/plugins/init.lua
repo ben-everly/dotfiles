d749349ccf1489d343f2a964954e922daae6ac4e
Ben Everly <ben.everly@oberd.com>
2023-07-03 21:28:25 -0500

    vim airline return table

--- config/nvim/lua/ben-everly/plugins/init.lua
+++ config/nvim/lua/ben-everly/plugins/init.lua
@@ -18,7 +18,7 @@ return packer.startup(function(use)
 	use { 'tpope/vim-fugitive', config = require 'ben-everly.plugins.vim-fugitive' }
 	use 'tpope/vim-rhubarb'
 	use { 'neoclide/coc.nvim', branch = 'release', config = require 'ben-everly.plugins.coc' }
-	use { 'vim-airline/vim-airline', config = require 'ben-everly.plugins.vim-airline' }
+	use(require 'ben-everly.plugins.vim-airline')
 	use(require 'ben-everly.plugins.vim-snippets')
 	use(require 'ben-everly.plugins.vim-yoink')
 	use(require 'ben-everly.plugins.nvim-treesitter')
--- config/nvim/lua/ben-everly/plugins/vim-airline.lua
+++ config/nvim/lua/ben-everly/plugins/vim-airline.lua
@@ -1,4 +1,7 @@
-return function()
-	vim.g.airline_powerline_fonts = 1
-	vim.g['airline#extensions#tabline#enabled'] = 1
-end
+return {
+	'vim-airline/vim-airline',
+	config = function()
+		vim.g.airline_powerline_fonts = 1
+		vim.g['airline#extensions#tabline#enabled'] = 1
+	end
+}
