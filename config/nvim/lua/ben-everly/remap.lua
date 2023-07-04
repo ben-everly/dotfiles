7b839c399590f9792408e10fe7904392aa5fde91
Ben Everly <ben.everly@oberd.com>
2023-07-04 06:24:21 -0500

    update mappings

--- config/nvim/lua/ben-everly/remap.lua
+++ config/nvim/lua/ben-everly/remap.lua
@@ -1,15 +1,10 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>w', ':w!<cr>')
-vim.keymap.set('n', '<leader>q', ':bd<cr>')
-vim.keymap.set('n', '<leader>l', ':ls<cr>')
-vim.keymap.set('n', '<leader><C-t>', ':enew<cr>')
-vim.keymap.set('n', '<leader><C-w>', ':bp|bd #<cr>')
-vim.keymap.set('n', '<leader>.', ':bnext<cr>')
-vim.keymap.set('n', '<leader>,', ':bprevious<cr>')
-vim.keymap.set('n', '<c-w><s-j>', ':res -10<cr>')
-vim.keymap.set('n', '<c-w><s-k>', ':res +10<cr>')
-vim.keymap.set('n', '<c-w><s-l>', ':vert res -10<cr>')
-vim.keymap.set('n', '<c-w><s-h>', ':vert res +10<cr>')
+vim.keymap.set('n', '<C-t>', vim.cmd.enew)
+vim.keymap.set('n', '<C-w>', ':bp|bd #<cr>')
+vim.keymap.set('n', '<c-]>', ':winc w<cr>')
+vim.keymap.set('n', '<c-[>', ':winc W<cr>')
+vim.keymap.set('n', '<leader>.', vim.cmd.bnext)
+vim.keymap.set('n', '<leader>,', vim.cmd.bprevious)
 vim.cmd([[
 vnoremap <silent> * :<C-U>
 \let old_reg=getreg('"')<Bar>let old_regtype=getregtype('"')<CR>
