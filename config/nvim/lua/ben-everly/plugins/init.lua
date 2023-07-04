af163110b48659f4c860a7002b848b8b375348da
Ben Everly <ben.everly@oberd.com>
2023-07-03 22:03:50 -0500

    formatting

--- config/nvim/lua/ben-everly/plugins/init.lua
+++ config/nvim/lua/ben-everly/plugins/init.lua
@@ -12,37 +12,37 @@ local packer_bootstrap = ensure_packer()
 
 local packer = require 'packer'
 return packer.startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'tpope/vim-sensible'
+	use('wbthomason/packer.nvim')
+	use('tpope/vim-sensible')
 	use(require 'ben-everly.plugins.gruvbox')
 	use(require 'ben-everly.plugins.vim-fugitive')
-	use 'tpope/vim-rhubarb'
+	use('tpope/vim-rhubarb')
 	use(require 'ben-everly.plugins.coc')
 	use(require 'ben-everly.plugins.vim-airline')
 	use(require 'ben-everly.plugins.vim-snippets')
 	use(require 'ben-everly.plugins.vim-yoink')
 	use(require 'ben-everly.plugins.nvim-treesitter')
-	use { 'nvim-treesitter/nvim-treesitter-context' }
+	use({ 'nvim-treesitter/nvim-treesitter-context' })
 	use(require 'ben-everly.plugins.nvim-treesitter-textobjects')
 	use(require 'ben-everly.plugins.nvim-dap')
 	use(require 'ben-everly.plugins.nvim-dap-ui')
-	use { 'tpope/vim-dadbod', ft = { 'sql', 'mysql' } }
-	use 'jiangmiao/auto-pairs'
-	use 'romainl/vim-qf'
+	use({ 'tpope/vim-dadbod', ft = { 'sql', 'mysql' } })
+	use('jiangmiao/auto-pairs')
+	use('romainl/vim-qf')
 	use(require 'ben-everly.plugins.vim-commentary')
-	use 'tpope/vim-surround'
-	use 'tpope/vim-sleuth'
-	use 'tpope/vim-repeat'
+	use('tpope/vim-surround')
+	use('tpope/vim-sleuth')
+	use('tpope/vim-repeat')
 	use(require 'ben-everly.plugins.telescope')
 	use(require 'ben-everly.plugins.telescope-file-browser')
 	use(require 'ben-everly.plugins.copilot')
 	use(require 'ben-everly.plugins.toggleterm')
-	use { 'mg979/vim-visual-multi', branch = 'master' }
-	use 'lambdalisue/suda.vim'
+	use({ 'mg979/vim-visual-multi', branch = 'master' })
+	use('lambdalisue/suda.vim')
 	use(require 'ben-everly.plugins.neotest')
 
 	-- treesitter doesn't have a grammar for blade yet
-	use 'jwalton512/vim-blade'
+	use('jwalton512/vim-blade')
 	if packer_bootstrap then
 		packer.sync()
 	else
