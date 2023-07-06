65b982a608c51b38b81b3f4076588376e1286728
Ben Everly <ben.everly@oberd.com>
2023-07-06 08:37:06 -0500

    change bnext, bprevious keymaps

--- config/nvim/lua/ben-everly/remap.lua
+++ config/nvim/lua/ben-everly/remap.lua
@@ -3,8 +3,8 @@ vim.keymap.set('n', '<C-t>', vim.cmd.enew)
 vim.keymap.set('n', '<C-w>', ':bp|bd #<cr>')
 vim.keymap.set('n', '<c-]>', ':winc w<cr>')
 vim.keymap.set('n', '<c-[>', ':winc W<cr>')
-vim.keymap.set('n', '<leader>.', vim.cmd.bnext)
-vim.keymap.set('n', '<leader>,', vim.cmd.bprevious)
+vim.keymap.set('n', '<TAB>', vim.cmd.bnext)
+vim.keymap.set('n', '<S-TAB>', vim.cmd.bprevious)
 vim.cmd([[
 vnoremap <silent> * :<C-U>
 \let old_reg=getreg('"')<Bar>let old_regtype=getregtype('"')<CR>
