02a145764a5ee975a16b01d48a1b332949a62df5
Ben Everly <ben.everly@oberd.com>
2023-07-15 10:45:39 -0500

    fix copilot default tab map

--- config/nvim/lua/ben-everly/plugins/copilot.lua
+++ config/nvim/lua/ben-everly/plugins/copilot.lua
@@ -1,7 +1,9 @@
 return {
 	'github/copilot.vim',
-	config = function()
+	init = function()
 		vim.g.copilot_no_tab_map = true
+	end,
+	config = function()
 		vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("")',
 			{ noremap = true, silent = true, expr = true, replace_keycodes = false })
 	end
