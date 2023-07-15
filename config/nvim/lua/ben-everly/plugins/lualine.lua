c6189817394eec090847a102aaba3bcd733daf4a
Ben Everly <ben.everly@oberd.com>
2023-07-14 23:30:21 -0500

    install fira code nerd font

--- config/nvim/lua/ben-everly/plugins/lualine.lua
+++ config/nvim/lua/ben-everly/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
 	'nvim-lualine/lualine.nvim',
-	requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+	requires = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
 		require 'lualine'.setup({
 			options = {
