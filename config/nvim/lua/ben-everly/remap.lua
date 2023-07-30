7742c983b4b4f335e9ea623e1b1d0958671398aa
Ben Everly <ben.everly@oberd.com>
2023-07-29 23:36:50 -0500

    window mode

--- config/nvim/lua/ben-everly/remap.lua
+++ config/nvim/lua/ben-everly/remap.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<C-t>', vim.cmd.enew)
 vim.keymap.set('n', '<C-w>', ':bp|bd #<cr>')
-vim.keymap.set('n', '<m-]>', ':winc w<cr>')
-vim.keymap.set('n', '<m-[>', ':winc W<cr>')
 vim.keymap.set('n', '<TAB>', vim.cmd.bnext)
 vim.keymap.set('n', '<S-TAB>', vim.cmd.bprevious)
 vim.cmd([[
@@ -17,3 +15,40 @@ vnoremap <silent> # :<C-U>
 \escape(@", '?\.*$^~['), '\_s\+', '\\_s\\+', 'g')<CR><CR>
 \gVzv:call setreg('"', old_reg, old_regtype)<CR>
 ]])
+
+vim.keymap.set('n', '<leader>w', function()
+	require'libmodal'.mode.enter('WINDOW', {
+		h = function()
+			vim.cmd('winc h')
+			require'lualine'.refresh()
+		end,
+		j = function()
+			vim.cmd('winc j')
+			require'lualine'.refresh()
+		end,
+		k = function()
+			vim.cmd('winc k')
+			require'lualine'.refresh()
+		end,
+		l = function()
+			vim.cmd('winc l')
+			require'lualine'.refresh()
+		end,
+		[']'] = function()
+			vim.cmd('winc w')
+			require'lualine'.refresh()
+		end,
+		['['] = function()
+			vim.cmd('winc W')
+			require'lualine'.refresh()
+		end,
+		n = function() vim.cmd('vsp') end,
+		s = function() vim.cmd('spl') end,
+		x = function() vim.cmd('q') end,
+		q = function() vim.cmd('q') end,
+		H = function() vim.cmd('vert res +2') end,
+		L = function() vim.cmd('vert res -2') end,
+		K = function() vim.cmd('res +2') end,
+		J = function() vim.cmd('res -2') end,
+	})
+end)
