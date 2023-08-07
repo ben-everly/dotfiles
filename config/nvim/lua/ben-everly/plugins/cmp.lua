bfda063a01856a022ee4627bfb544df2d38afed8
Ben Everly <ben.everly@oberd.com>
2023-08-06 22:06:34 -0500

    map esc to close cmp if open

--- config/nvim/lua/ben-everly/plugins/cmp.lua
+++ config/nvim/lua/ben-everly/plugins/cmp.lua
@@ -44,15 +44,15 @@ return {
 				['<C-k>'] = cmp.mapping.scroll_docs(-4),
 				['<C-j>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = function()
-				if cmp.visible() then
-					cmp.mapping.abort()
+					if cmp.visible() then
+						cmp.mapping.abort()
 					else
 						cmp.mapping.complete()
 					end
 				end,
 				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				['<Tab>'] = function(fallback)
-				if cmp.visible() then
+					if cmp.visible() then
 						cmp.select_next_item()
 					elseif not previous_char_is_whitespace() then
 						cmp.complete()
@@ -61,7 +61,7 @@ return {
 					end
 				end,
 				['<S-Tab>'] = function(fallback)
-				if cmp.visible() then
+					if cmp.visible() then
 						cmp.select_prev_item()
 					elseif not previous_char_is_whitespace() then
 						cmp.complete()
@@ -69,6 +69,13 @@ return {
 						fallback()
 					end
 				end,
+				['<Esc>'] = function(fallback)
+					if cmp.visible() then
+						cmp.close()
+					else
+						fallback()
+					end
+				end
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
