e476b0fd878dce0a4725de013f9128b71787155f
Ben Everly <ben.everly@oberd.com>
2023-08-31 22:14:29 -0500

    border on floating diagnostic window

--- config/nvim/lua/ben-everly/plugins/lspconfig.lua
+++ config/nvim/lua/ben-everly/plugins/lspconfig.lua
@@ -65,7 +65,10 @@ return {
 				prefix = "",
 			},
 			severity_sort = true,
-			float = { float = "if_many" },
+			float = {
+				float = "if_many",
+				border = "single"
+			},
 		})
 		vim.api.nvim_create_autocmd('LspAttach', {
 			group = vim.api.nvim_create_augroup('UserLspConfig', {}),
