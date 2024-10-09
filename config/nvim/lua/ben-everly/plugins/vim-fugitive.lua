871e8d0c14c06db26b3b45f2d8b0c4fdf82fa182
Ben Everly <ben.everly@oberd.com>
2024-10-09 13:21:52 -0500

    open fugitive statis in vertical split

--- config/nvim/lua/ben-everly/plugins/vim-fugitive.lua
+++ config/nvim/lua/ben-everly/plugins/vim-fugitive.lua
@@ -1,7 +1,7 @@
 return {
 	'tpope/vim-fugitive',
 	config = function()
-		vim.keymap.set('n', '<leader>gs', ':Git<CR>')
+		vim.keymap.set('n', '<leader>gs', ':vert rightb Git<CR>')
 		vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
 		vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 		vim.keymap.set('n', '<leader>gf', ':Git fetch<CR>')
