3e0cd67cd452a9783c58056338aa354cc331f80a
Ben Everly <ben.everly@oberd.com>
2024-10-09 14:58:42 -0500

    add neogit plugin

---
+++ config/nvim/lua/ben-everly/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+	'NeogitOrg/neogit',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'sindrets/diffview.nvim',
+		'nvim-telescope/telescope.nvim',
+	},
+	config = function ()
+		require 'neogit'.setup {
+			kind = "auto",
+		}
+		vim.keymap.set('n', '<leader>gs', require('neogit').open)
+	end
+}
