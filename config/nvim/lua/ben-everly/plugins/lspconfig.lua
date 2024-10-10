c64c1feac952fbe37ca475af8b4269884834ed55
Ben Everly <ben.everly@oberd.com>
2024-10-10 15:02:06 -0500

    use sqls in place of dadbod

--- config/nvim/lua/ben-everly/plugins/lspconfig.lua
+++ config/nvim/lua/ben-everly/plugins/lspconfig.lua
@@ -127,6 +127,13 @@ return {
 					},
 				}
 			end,
+			['sqls'] = function ()
+				lspconfig.sqls.setup{
+					on_attach = function(client, bufnr)
+						require('sqls').on_attach(client, bufnr)
+					end
+				}
+			end,
 		}
 	end,
 }
--- config/nvim/lua/ben-everly/plugins/vim-dadbod.lua
+++
@@ -1,4 +0,0 @@
-return {
-	'tpope/vim-dadbod',
-	ft = { 'sql', 'mysql' },
-}
