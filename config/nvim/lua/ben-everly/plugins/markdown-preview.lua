988d47e7b225682add0544e9464915d11d8882c3
Ben Everly <ben.everly@oberd.com>
2024-11-04 11:56:23 -0600

    use yarn instead of npm, and add cmds for lazy loading

--- config/nvim/lua/ben-everly/plugins/markdown-preview.lua
+++ config/nvim/lua/ben-everly/plugins/markdown-preview.lua
@@ -1,6 +1,7 @@
 return {
 	"iamcco/markdown-preview.nvim",
-	build = "cd app && npm install",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
 	end,
