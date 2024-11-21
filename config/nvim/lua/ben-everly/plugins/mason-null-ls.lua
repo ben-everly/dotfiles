5959afbe1d68f07ecb222560766a71f924e32936
Ben Everly <ben.everly@oberd.com>
2024-11-21 16:34:34 -0600

    Fix dependencies for mason-null-ls

--- config/nvim/lua/ben-everly/plugins/mason-null-ls.lua
+++ config/nvim/lua/ben-everly/plugins/mason-null-ls.lua
@@ -1,8 +1,10 @@
 return {
-	'jay-babu/mason-null-ls.nvim',
-	dependencies = {
-		'williambowman/mason.nvim',
-	},
+    'jay-babu/mason-null-ls.nvim',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
 	config = function()
 		require('mason-null-ls').setup {
 			handlers = {}
--- config/nvim/lua/ben-everly/plugins/none-ls.lua
+++ config/nvim/lua/ben-everly/plugins/none-ls.lua
@@ -1,8 +1,5 @@
 return {
 	'nvimtools/none-ls.nvim',
-	dependencies = {
-		'jay-babu/mason-null-ls.nvim',
-	},
 	config = function()
 		require'null-ls'.setup {
 			border = 'single',
