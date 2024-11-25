6f7d7ee71a6a3dbfc6627b7ffeb559102fd7eada
Ben Everly <ben.everly@oberd.com>
2024-11-25 17:12:54 -0600

    Move server specific setups into mason-lspconifg add ensure_installed

--- config/nvim/lua/ben-everly/plugins/lspconfig.lua
+++ config/nvim/lua/ben-everly/plugins/lspconfig.lua
@@ -94,29 +94,5 @@ return {
 				end)
 			end,
 		})
-		local lspconfig = require 'lspconfig'
-		require('mason-lspconfig').setup_handlers {
-			function(server_name)
-				lspconfig[server_name].setup {}
-			end,
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" }
-							},
-						},
-					},
-				}
-			end,
-			['sqls'] = function ()
-				lspconfig.sqls.setup{
-					on_attach = function(client, bufnr)
-						require('sqls').on_attach(client, bufnr)
-					end
-				}
-			end,
-		}
 	end,
 }
--- config/nvim/lua/ben-everly/plugins/mason-lspconfig.lua
+++ config/nvim/lua/ben-everly/plugins/mason-lspconfig.lua
@@ -1,6 +1,36 @@
 return {
 	'williamboman/mason-lspconfig.nvim',
 	config = function()
-		require('mason-lspconfig').setup {}
+		require('mason-lspconfig').setup({
+			ensure_installed = {
+				'ansiblels',
+				'cssls',
+				'docker_compose_language_service',
+				'dockerls',
+				'eslint',
+				'html',
+				'intelephense',
+				'jsonls',
+				'lemminx',
+				'lua_ls',
+				'sqls',
+				'tailwindcss',
+				'ts_ls',
+				'yamlls',
+			}
+		})
+		local lspconfig = require('lspconfig')
+		require('mason-lspconfig').setup_handlers {
+			function(server_name)
+				lspconfig[server_name].setup {}
+			end,
+			['sqls'] = function ()
+				lspconfig.sqls.setup{
+					on_attach = function(client, bufnr)
+						require('sqls').on_attach(client, bufnr)
+					end
+				}
+			end,
+		}
 	end,
 }
