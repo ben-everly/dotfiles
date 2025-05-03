4bce5a7b13f8f728fba9addd8842db27caa7b070
Ben Everly <ben.everly@oberd.com>
2025-05-03 17:09:59 -0500

    vectorcode through codecompanion

--- config/nvim/lua/ben-everly/plugins/codecompanion.lua
+++ config/nvim/lua/ben-everly/plugins/codecompanion.lua
@@ -1,18 +1,21 @@
 return {
 	'olimorris/codecompanion.nvim',
-	opts = {},
+	opts = {
+		extensions = {
+			vectorcode = {
+				opts = { add_tool = true, add_slash_command = true, tool_opts = {} },
+			},
+		}
+	},
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		'nvim-treesitter/nvim-treesitter',
+		'Davidyz/VectorCode',
 	},
-	config = function()
-		require('codecompanion').setup({
-			vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true });
-			vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true });
-			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true });
-
-			-- Expand 'cc' into 'CodeCompanion' in the command line
-			vim.cmd([[cab cc CodeCompanion]]);
-		})
+	init = function()
+		vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+		vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+		vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+		vim.cmd([[cab cc CodeCompanion]])
 	end,
 }
---
+++ config/nvim/lua/ben-everly/plugins/vectorcode.lua
@@ -0,0 +1,6 @@
+return {
+	"Davidyz/VectorCode",
+	version = "*",
+	build = "pipx upgrade vectorcode",
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
