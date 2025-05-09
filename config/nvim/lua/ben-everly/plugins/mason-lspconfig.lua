9367f66586a19422c98947a064e3f0a68646a2f6
Ben Everly <ben.everly@oberd.com>
2025-05-09 09:27:42 -0500

    Remove unneeded mason-lspconfig setup

--- config/nvim/lua/ben-everly/plugins/mason-lspconfig.lua
+++ config/nvim/lua/ben-everly/plugins/mason-lspconfig.lua
@@ -19,18 +19,5 @@ return {
 				'yamlls',
 			}
 		})
-		local lspconfig = require('lspconfig')
-		require('mason-lspconfig').setup_handlers {
-			function(server_name)
-				lspconfig[server_name].setup {}
-			end,
-			['sqls'] = function ()
-				lspconfig.sqls.setup{
-					on_attach = function(client, bufnr)
-						require('sqls').on_attach(client, bufnr)
-					end
-				}
-			end,
-		}
 	end,
 }
