61033ba0eeff61f1c794101f578b069d98d60d84
Ben Everly <ben.everly@oberd.com>
2025-06-03 20:13:50 -0500

    Use lowercase for global marks

--- config/nvim/lua/ben-everly/remap.lua
+++ config/nvim/lua/ben-everly/remap.lua
@@ -20,3 +20,13 @@ local qf_group = vim.api.nvim_create_augroup('qf', { clear = true })
 vim.api.nvim_create_autocmd('FileType', { pattern = 'qf', group = qf_group, callback = function()
 	vim.keymap.set("n", "<CR>", "<CR>:ccl<CR>", { buffer = true })
 end })
+
+local low = function(i) return string.char(97+i) end
+local upp = function(i) return string.char(65+i) end
+
+for i=0,25 do vim.keymap.set("n", "m"..low(i), "m"..upp(i)) end
+for i=0,25 do vim.keymap.set("n", "m"..upp(i), "m"..low(i)) end
+for i=0,25 do vim.keymap.set("n", "'"..low(i), "'"..upp(i)) end
+for i=0,25 do vim.keymap.set("n", "'"..upp(i), "'"..low(i)) end
+for i=0,25 do vim.keymap.set("n", "`"..low(i), "`"..upp(i)) end
+for i=0,25 do vim.keymap.set("n", "`"..upp(i), "`"..low(i)) end
