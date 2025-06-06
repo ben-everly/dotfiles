1202c2f9b3d4594d611cc18a9adb76802fa046c6
Ben Everly <ben.everly@oberd.com>
2025-06-06 13:01:03 -0500

    Detect git for find files keymap

--- config/nvim/lua/ben-everly/plugins/telescope.lua
+++ config/nvim/lua/ben-everly/plugins/telescope.lua
@@ -3,8 +3,14 @@ return {
 	dependencies = 'nvim-lua/plenary.nvim',
 	config = function()
 		local builtin = require 'telescope.builtin'
-		vim.keymap.set('n', '<leader>ff', builtin.find_files)
-		vim.keymap.set('n', '<leader>fF', builtin.git_files)
+		vim.keymap.set('n', '<leader>ff', function ()
+			if vim.fn.finddir('.git', vim.fn.getcwd() .. ';') ~= '' then
+				builtin.git_files()
+			else
+				builtin.find_files()
+			end
+		end)
+		vim.keymap.set('n', '<leader>fF', builtin.find_files)
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 		vim.keymap.set('n', '<leader>fb', builtin.buffers)
 		vim.keymap.set('n', '<leader>fm', builtin.marks)
@@ -40,6 +46,10 @@ return {
 				},
 			},
 			pickers = {
+				find_files = {
+					hidden = true,
+					no_ignore = true,
+				},
 				marks = {
 					mappings = {
 						i = {
