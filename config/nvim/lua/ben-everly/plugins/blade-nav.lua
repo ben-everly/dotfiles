ba8580f75eb96e0d29bdd60907c271a874ab3e67
Ben Everly <ben.everly@oberd.com>
2025-06-21 20:02:58 -0500

    blade-nav.nvim plugin

---
+++ config/nvim/lua/ben-everly/plugins/blade-nav.lua
@@ -0,0 +1,11 @@
+return
+{
+  'ricardoramirezr/blade-nav.nvim',
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+  },
+  ft = { 'blade', 'php' },
+  opts = {
+    close_tag_on_complete = true,
+  },
+}
