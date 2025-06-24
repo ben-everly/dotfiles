21f8086028f2a6864856ad6a5ea22761d79dc9c3
Ben Everly <ben.everly@oberd.com>
2025-06-24 15:43:33 -0500

    update avante config to use cursor apply mode, providers for groq and ollama

--- config/nvim/lua/ben-everly/plugins/avante.lua
+++ config/nvim/lua/ben-everly/plugins/avante.lua
@@ -3,30 +3,35 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
-    -- for example
     provider = "copilot",
-    -- openai = {
-    --   endpoint = "https://api.openai.com/v1",
-    --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    --   temperature = 0,
-    --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-    -- },
+    providers = {
+      groq = {
+        __inherited_from = "openai",
+        api_key_name = "GROQ_API_KEY",
+        endpoint = "https://api.groq.com/openai/v1/",
+        model = "llama-3.3-70b-versatile",
+      },
+      ollama = {
+        endpoint = "http://localhost:11434",
+        model = "mistral:7b",
+      },
+    },
+    -- cursor_applying_provider = "ollama",
+    cursor_applying_provider = "groq",
+    behaviour = {
+      enable_cursor_planning_mode = true,
+    },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
