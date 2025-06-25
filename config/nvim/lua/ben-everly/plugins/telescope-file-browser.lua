0dfe717174c1e94280640bb9f2964acbdaa837ad
Ben Everly <ben.everly@oberd.com>
2025-06-24 21:45:40 -0500

    add some options for telescope-file-browser

--- config/nvim/lua/ben-everly/plugins/telescope-file-browser.lua
+++ config/nvim/lua/ben-everly/plugins/telescope-file-browser.lua
@@ -2,9 +2,19 @@ return {
 	'nvim-telescope/telescope-file-browser.nvim',
 	dependencies = 'telescope.nvim',
 	config = function()
-		vim.keymap.set('n', '<space>ft', require 'telescope'.extensions.file_browser.file_browser, { noremap = true })
+		vim.keymap.set('n', '<space>ft', function()
+			require 'telescope'.extensions.file_browser.file_browser {
+				grouped = true,
+				collapse_dirs = true,
+			}
+		end, { noremap = true })
 		vim.keymap.set('n', '<space>fT', function()
-			require 'telescope'.extensions.file_browser.file_browser { path = '%:p:h', select_buffer = true }
+			require 'telescope'.extensions.file_browser.file_browser {
+				grouped = true,
+				collapse_dirs = true,
+				path = '%:p:h',
+				select_buffer = true
+			}
 		end, { noremap = true })
 	end
 }
