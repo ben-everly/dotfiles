18fad9e307abd0b2699bb1cc3a1e89f935c79cb3
Ben Everly <ben.everly@oberd.com>
2025-07-04 09:53:27 -0500

    typehinting for avante config

--- config/nvim/lua/ben-everly/plugins/avante.lua
+++ config/nvim/lua/ben-everly/plugins/avante.lua
@@ -2,6 +2,8 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
   opts = {
     provider = "copilot",
     providers = {
