5e5f5626c9af874f5939ea07fcec6264c50a4fa5
Ben Everly <ben.everly@oberd.com>
2025-12-30 22:05:48 -0600

    config phpmd with xml

--- config/nvim/lua/ben-everly/plugins/none-ls.lua
+++ config/nvim/lua/ben-everly/plugins/none-ls.lua
@@ -8,6 +8,8 @@ return {
 					extra_args = function()
 						if vim.fn.filereadable("phpmd.xml") == 1 then
 							return { "phpmd.xml" }
+						elseif vim.fn.filereadable(vim.env.HOME .. "/.config/phpmd/phpmd.xml") == 1 then
+							return { vim.env.HOME .. "/.config/phpmd/phpmd.xml" }
 						else
 							return { "cleancode,codesize,controversial,design,naming,unusedcode" }
 						end
