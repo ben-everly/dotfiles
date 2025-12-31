8f4adb6f417a080894fb408a0ab8f347cac53144
Ben Everly <ben.everly@oberd.com>
2025-12-30 22:19:59 -0600

    set foldlevel instead of disabling folds

--- config/nvim/lua/ben-everly/plugins/nvim-treesitter.lua
+++ config/nvim/lua/ben-everly/plugins/nvim-treesitter.lua
@@ -105,6 +105,7 @@ return {
 		})
 		vim.opt.foldmethod = "expr"
 		vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-		vim.opt.foldenable = false
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
 	end,
 }
