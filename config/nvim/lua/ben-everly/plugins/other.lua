bc62c5491671528287e394140f43f6be4eafa5d7
Ben Everly <ben.everly@oberd.com>
2026-02-09 14:10:24 -0600

    feat(nvim): refine livewire mappings for other-nvim

--- config/nvim/lua/ben-everly/plugins/other.lua
+++ config/nvim/lua/ben-everly/plugins/other.lua
@@ -3,8 +3,31 @@ return {
 	config = function()
 		require("other-nvim").setup({
 			mappings = {
-				"livewire",
 				"laravel",
+				{
+					pattern = "/app/Livewire/(.*).php",
+					target = "/resources/views/livewire/%1.blade.php",
+					context = "view",
+					transformer = "camelToKebap",
+				},
+				{
+					pattern = "/app/Livewire/(.*).php",
+					target = "/resources/views/livewire/%1/",
+					context = "view",
+					transformer = "camelToKebap",
+				},
+				{
+					pattern = "/resources/views/livewire/(.*).blade.php",
+					target = "/app/Livewire/%1.php",
+					context = "component",
+					transformer = "kebapToCamel",
+				},
+				{
+					pattern = "/resources/views/livewire/(.*)/.*.blade.php",
+					target = "/app/Livewire/%1.php",
+					context = "component",
+					transformer = "kebapToCamel",
+				},
 				{
 					pattern = "/app/(.*)/(.*).php",
 					target = "/tests/Unit/app/%1/%2Test.php",
