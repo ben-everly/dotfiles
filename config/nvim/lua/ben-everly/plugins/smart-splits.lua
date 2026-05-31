02706a12ea10adbeb185930292b732c0d7dc0283
Ben Everly <ben.everly@oberd.com>
2026-05-31 15:21:35 -0500

    feat(nvim): allow smart-splits navigation from visual mode
    Map <C-h/j/k/l> in visual mode so a selection can be kept while
    switching to another wezterm pane.

--- config/nvim/lua/ben-everly/plugins/smart-splits.lua
+++ config/nvim/lua/ben-everly/plugins/smart-splits.lua
@@ -5,10 +5,10 @@ return {
 		ss.setup({
 			multiplexer_integration = "wezterm",
 		})
-		vim.keymap.set("n", "<C-h>", ss.move_cursor_left, { desc = "Move to split/pane left" })
-		vim.keymap.set("n", "<C-j>", ss.move_cursor_down, { desc = "Move to split/pane below" })
-		vim.keymap.set("n", "<C-k>", ss.move_cursor_up, { desc = "Move to split/pane above" })
-		vim.keymap.set("n", "<C-l>", ss.move_cursor_right, { desc = "Move to split/pane right" })
+		vim.keymap.set({ "n", "x" }, "<C-h>", ss.move_cursor_left, { desc = "Move to split/pane left" })
+		vim.keymap.set({ "n", "x" }, "<C-j>", ss.move_cursor_down, { desc = "Move to split/pane below" })
+		vim.keymap.set({ "n", "x" }, "<C-k>", ss.move_cursor_up, { desc = "Move to split/pane above" })
+		vim.keymap.set({ "n", "x" }, "<C-l>", ss.move_cursor_right, { desc = "Move to split/pane right" })
 		vim.keymap.set("n", "<A-h>", ss.resize_left, { desc = "Resize split/pane left" })
 		vim.keymap.set("n", "<A-j>", ss.resize_down, { desc = "Resize split/pane down" })
 		vim.keymap.set("n", "<A-k>", ss.resize_up, { desc = "Resize split/pane up" })
