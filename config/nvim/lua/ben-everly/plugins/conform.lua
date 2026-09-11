45db3a11e40a4e6bfad9c2b6706b50dbae5ed4db
Ben Everly <benjamin.g.everly@gmail.com>
2026-09-10 19:52:45 -0500

    feat(nvim): format vue files with prettierd

--- config/nvim/lua/ben-everly/plugins/conform.lua
+++ config/nvim/lua/ben-everly/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
 			javascriptreact = { "prettierd" },
 			typescript = { "prettierd" },
 			typescriptreact = { "prettierd" },
+			vue = { "prettierd" },
 		},
 		formatters = {
 			injected = {
